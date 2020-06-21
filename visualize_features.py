ac9fe2a30506e0d608ee7ff2d3e4c01da3bcc0d5
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-06-21 15:08:05 +0200

    Refactor argument-parser in visualize_features.py

--- visualize_features.py
+++ visualize_features.py
@@ -1,9 +1,16 @@
 """A script for visualizing features in a CSV file in different ways
 
-Specifically:
- 1. It applies dimensionality-reduction using PCA
- 2. Clusters using T-SNE
- 3.  Writes a visualization of the clustering to the filesystem
+It first projects the features into an embedding, and then visualizes the embedding using different methods.
+
+Projection methods
+------------------
+ * T-SNE (default)
+ * PCA,
+
+Visusalization methods
+----------------------
+ * a 2D interactive plot of points in ploty, opens in the web-browser (default)
+ * exports a log-dir which can be opened in TensorBoard
 
 Structure of the CSV File
 -------------------------
@@ -23,7 +30,7 @@ Author
 Owen Feehan
 """
 import argparse
-from typing import Optional
+from typing import List
 
 from features import *
 from projection import *
@@ -47,20 +54,21 @@ def main():
 def _arg_parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Visualize a CSV file with different features.')
     parser.add_argument('file_path_to_csv', type=str, help='file-path to a csv file')
-    parser.add_argument(
+    _add_method_via_choices(
+        parser,
         "-m",
         "--method",
-        help="method to use for visualization. Defaults to '{}'"
-        .format(VISUALIZE_FEATURES_DEFAULT_IDENTIFIER),
-        choices=VISUALIZE_FEATURES_FACTORY_IDENTIFIERS,
-        default=VISUALIZE_FEATURES_DEFAULT_IDENTIFIER
+        VISUALIZE_FEATURES_FACTORY_IDENTIFIERS,
+        VISUALIZE_FEATURES_DEFAULT_IDENTIFIER,
+        "visualization"
     )
-    parser.add_argument(
+    _add_method_via_choices(
+        parser,
+        "-p",
         "--projection",
-        help="method to use for projecting features to smaller dimensionality. Defaults to '{}'"
-        .format(PROJECTION_FACTORY_DEFAULT_IDENTIFIER),
-        choices=PROJECTION_FACTORY_IDENTIFIERS,
-        default=PROJECTION_FACTORY_DEFAULT_IDENTIFIER
+        PROJECTION_FACTORY_IDENTIFIERS,
+        PROJECTION_FACTORY_DEFAULT_IDENTIFIER,
+        "projecting features to smaller dimensionality"
     )
     parser.add_argument(
         "-o",
@@ -68,6 +76,24 @@ def _arg_parse() -> argparse.ArgumentParser:
         help="path to write any output to for a particular visualization method"
     )
     return parser.parse_args()
+
+
+def _add_method_via_choices(
+    parser: argparse.ArgumentParser,
+    short_name: str,
+    long_name: str,
+    choices: List[str],
+    default_choice: str,
+    help_msg: str
+) -> None:
+    """Adds a multiple-choice method to the parser"""
+    parser.add_argument(
+        short_name,
+        long_name,
+        help="Method to use for {}. Defaults to '{}'".format(help_msg,default_choice),
+        choices=choices,
+        default=default_choice
+    )
 
 
 if __name__ == "__main__":
