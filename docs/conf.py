a082e0d00b6627bdf63dcbf2018a6ee2ea430633
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-01-16 20:08:51 +0100

    Added base URL for anchor website to sphinx docs.

--- docs/conf.py
+++ docs/conf.py
@@ -86,10 +86,12 @@ html_theme_options = {"sidebarwidth": "15%", "body_max_width": "80%"}
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-
 # -- Extension configuration -------------------------------------------------
 
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# So stylesheets are loaded correctly when placed on the anchor-analysis website
+html_baseurl = 'https://www.anchoranalysis.org/anchor-python-visualization/'
