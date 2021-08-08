5febfe1ef4045890e2bce00b8b97647d6b7cb12f
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-08-08 19:54:09 +0200

    improved type annotations and doc in _derive_utilities.py

--- projection/_derive_utilities.py
+++ projection/_derive_utilities.py
@@ -9,16 +9,16 @@ import numpy as np
 import pandas as pd
 
 
-def derive_projected(unprojected: pd.DataFrame, projected: np.array, feature_prefix: str):
+def derive_projected(unprojected: pd.DataFrame, projected: np.array, feature_prefix: str) -> pd.DataFrame:
     """
     Converts a projected numpy array (derived from a data-frame) back into data-frame format with row.names
 
     :param unprojected: the original data-frame from which the projection was derived
-    :param projected: the projection derived from `unprojected`
-    :param feature_prefix: a prefix to use in feature names to become `feature_prefix0`, `feature_prefix1` etc.
-    :return: returns a data-frame with identical row order and sze to df_orig but with embeddings from projection
+    :param projected: the projection derived from :code:`unprojected`
+    :param feature_prefix: a prefix to use in feature names e.g. :code:`feature_prefix0`, :code:`feature_prefix1` etc.
+    :returns: a data-frame with identical row order and size to :code:`df_orig` but with embeddings from the projection.
     """
-    num_cols = np.size(projected, 1)
+    num_columns = np.size(projected, 1)
     return pd.DataFrame(
-        projected, columns=["%s%i" % (feature_prefix, i) for i in range(num_cols)], index=unprojected.index
+        projected, columns=["%s%i" % (feature_prefix, i) for i in range(num_columns)], index=unprojected.index
     )
