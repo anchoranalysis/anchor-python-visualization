3fea95618d74af6e01a99479747aa1a0aa35cffe
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-11-16 17:19:49 +0100

    anchor-python-visualization#14 restored missing files, changed to underscore rather than hyphen in src/ directory

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-    Setup file for anchor-python-training.
+    Setup file for anchor-python-visualization.
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 4.0.2.
