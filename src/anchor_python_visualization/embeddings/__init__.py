a7b7e5a641a2597fb546a6949259a7f03ac57863
Owen Feehan <owenfeehan@users.noreply.github.com>
2023-06-17 09:11:59 +0200

    black and isort

--- src/anchor_python_visualization/embeddings/__init__.py
+++ src/anchor_python_visualization/embeddings/__init__.py
@@ -1,8 +1,6 @@
 """Methods for loading embeddings and determining labels."""
-from anchor_python_visualization.embeddings.exceptions import \
-    InsufficientRowsException
-from anchor_python_visualization.embeddings.features import (
-    COLUMN_NAME_IDENTIFIER, PLACEHOLDER_FOR_SUBSTITUTION, load_features)
+from anchor_python_visualization.embeddings.exceptions import InsufficientRowsException
+from anchor_python_visualization.embeddings.features import COLUMN_NAME_IDENTIFIER, PLACEHOLDER_FOR_SUBSTITUTION, load_features
 from anchor_python_visualization.embeddings.label import LabelledFeatures
 
 __all__ = [
