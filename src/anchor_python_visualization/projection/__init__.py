e7e8f8758ec68b85d3935ed59557c57f61b239c0
Owen Feehan <owenfeehan@users.noreply.github.com>
2023-06-17 08:42:39 +0200

    black

--- src/anchor_python_visualization/projection/__init__.py
+++ src/anchor_python_visualization/projection/__init__.py
@@ -1,7 +1,9 @@
 """Methods for projecting a feature space to lower dimensionality."""
-from anchor_python_visualization.projection.factory import (DEFAULT_IDENTIFIER,
-                                                            IDENTIFIERS,
-                                                            create_projector)
+from anchor_python_visualization.projection.factory import (
+    DEFAULT_IDENTIFIER,
+    IDENTIFIERS,
+    create_projector,
+)
 from anchor_python_visualization.projection.projector import Projector
 
 __all__ = ["DEFAULT_IDENTIFIER", "IDENTIFIERS", "create_projector", "Projector"]
